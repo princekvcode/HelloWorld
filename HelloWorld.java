26a96f61033d8d3124424e0d67d4e5c0ae99528c
princekvcode <99193556+princekvcode@users.noreply.github.com>
2023-04-11 10:48:14 -0400

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		//Printing Hello World
+		System.out.println("It's a good day!");
+		
 
 	}
 
